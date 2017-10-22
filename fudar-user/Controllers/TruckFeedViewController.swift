b5de6c6c3da34b3fbdc91ae7a244bd386cea9406
Cristina <ce@tsl.io>
2017-10-22 04:16:51 -0700

    added images

--- fudar-user/Controllers/TruckFeedViewController.swift
+++ fudar-user/Controllers/TruckFeedViewController.swift
@@ -17,15 +17,15 @@ final class TruckFeedViewController: UIViewController {
     // MARK: - Private Instance Variables
     //var trucks: [Truck] = []
     var trucks: [[String: Any]] = [
-        ["name" : "Bluth's Frozen Banana Stand", "type" : "Dessert", "image" : "bluth_logo", "rating": 2, "reviews" : 42],
-        ["name" : "Naan Stop", "type" : "Indian", "image" : "Naan_logo", "rating": 4, "reviews" : 25],
-        ["name" : "Truck Norris", "type" : "Fried Chicken", "image" : "nunchicken", "rating": 4, "reviews" : 17],
-        ["name" : "I Dream of Weenie", "type" : "Hot Dogs and so forth", "image" : "weenie_logo", "rating": 4, "reviews" : 17],
+        ["name" : "Bluth's Frozen Banana Stand", "type" : "Dessert", "image" : "Bluths Orig", "rating": 2, "reviews" : 42],
+        ["name" : "Naan Stop", "type" : "Indian", "image" : "NaanStop", "rating": 4, "reviews" : 25],
+        ["name" : "Truck Norris", "type" : "Fried Chicken", "image" : "TruckNorrisChicken", "rating": 4, "reviews" : 17],
+        ["name" : "I Dream of Weenie", "type" : "Hot Dogs and so forth", "image" : "IDreamofWeenie", "rating": 4, "reviews" : 17],
+        ["name" : "Slaw & Order", "type" : "BBQ", "image" : "Slaw&Order", "rating": 5, "reviews" : 73],
         ["name" : "Easy Slider", "type" : "Burgers", "image" : "easy_logo", "rating": 4, "reviews" : 13],
         ["name" : "Starchy & Husk", "type" : "Gourmet Corn on the Cob", "image" : "corny", "rating": 3, "reviews" : 25],
         ["name" : "Pasta Dutchie", "type" : "Northen Italian", "image" : "spaghetti", "rating": 5, "reviews" : 35],
-        ["name" : "General Cheezious", "type" : "Grilled Cheese", "image" : "cheese", "rating": 4, "reviews" : 43],
-        ["name" : "Slaw & Order", "type" : "BBQ", "image" : "BBQ", "rating": 5, "reviews" : 73]
+        ["name" : "General Cheezious", "type" : "Grilled Cheese", "image" : "cheese", "rating": 4, "reviews" : 43]
     ]
 
     // MARK: - ViewLifeCycle
